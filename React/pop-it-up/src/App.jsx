64280d1a930914e31ba57e5338479c9e631dd624
0-Abdallah-Awwad-0 <abdallah22awwad11@gmail.com>
2026-07-30 05:50:34 +0300

    pop it up

---
+++ React/pop-it-up/src/App.jsx
@@ -0,0 +1,39 @@
+import PersonCard from "./PersonCard";
+
+function App() {
+  return (
+    <div>
+      <h1>Person Cards</h1>
+
+      <PersonCard
+        firstName="Jane"
+        lastName="Doe"
+        age={45}
+        hairColor="Black"
+      />
+
+      <PersonCard
+        firstName="John"
+        lastName="Smith"
+        age={88}
+        hairColor="Brown"
+      />
+
+      <PersonCard
+        firstName="Millard"
+        lastName="Fillmore"
+        age={50}
+        hairColor="Brown"
+      />
+
+      <PersonCard
+        firstName="Maria"
+        lastName="Smith"
+        age={62}
+        hairColor="Brown"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/pop-it-up/src/PersonCard.jsx
@@ -0,0 +1,15 @@
+function PersonCard({ firstName, lastName, age, hairColor }) {
+  return (
+    <div>
+      <h2>
+        {lastName}, {firstName}
+      </h2>
+
+      <p>Age: {age}</p>
+      <p>Hair Color: {hairColor}</p>
+      <hr />
+    </div>
+  );
+}
+
+export default PersonCard;
